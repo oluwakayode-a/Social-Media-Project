af518b47587bd4f28906db850a9d198a47f10d2e
oluwakayode-a <khaox07@gmail.com>
2020-10-11 17:58:48 +0100

    DEPLOY: Ready to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn earthruh.wsgi --log-file -
--- earthruh/settings.py
+++ earthruh/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k@xc)*z&l5h7ewp&i^@*l#pa^r=(cb--j#r5+ljay2$3p+iq3p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['earthruh.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -154,5 +154,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
