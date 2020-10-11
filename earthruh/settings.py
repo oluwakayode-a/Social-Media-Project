5a3d6327e9ea97a0384fd877c4789b6308aac3bf
oluwakayode-a <khaox07@gmail.com>
2020-10-11 21:22:39 +0100

    edited files

--- earthruh/settings.py
+++ earthruh/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k@xc)*z&l5h7ewp&i^@*l#pa^r=(cb--j#r5+ljay2$3p+iq3p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['earthruh.herokuapp.com', 'localhost', '127.0.0.1']
 
